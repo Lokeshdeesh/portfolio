a362fa0882e18f33a4dd35cedaccaebfdc5b4dd2
Lokeshdeesh <168923849+Lokeshdeesh@users.noreply.github.com>
2024-06-07 23:00:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-document.getElementById("nav_toggle").addEventListener("click", () => {
-    document.getElementById("nav_menu").classList.toggle("show");
-  });
-  
 // SHOW MENU
 
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-          nav = document.getElementById(navId)
+      const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
 
-    if(toggle && nav){
-          toggle.addEventListener('click', () =>{
-                nav.classList.toggle('show')
-          });
-    }
+      if(toggle && nav){
+            toggle.addEventListener('click', () =>{
+                  nav.classList.toggle('show')
+            });
+      }
 }
 
 showMenu('nav_toggle','nav_menu')
@@ -22,11 +18,11 @@ const navLink = document.querySelectorAll('.nav_link')
 navLink.forEach(n => n.classList.remove('active'))
 
 function linkAction(){
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
+      navLink.forEach(n => n.classList.remove('active'))
+      this.classList.add('active')
 
-    const navMenu = document.getElementById('nav_menu')
-    navMenu.classList.remove('show')
+      const navMenu = document.getElementById('nav_menu')
+      navMenu.classList.remove('show')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
